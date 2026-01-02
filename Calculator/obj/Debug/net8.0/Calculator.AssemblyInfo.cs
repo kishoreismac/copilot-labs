43e2d9ca8321013c5d7874a23f43dcf1e4843e63
kishoreismac <27953059+kishoreismac@users.noreply.github.com>
2026-01-02 03:28:21 +0000

    added sln file

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc7f2927ab73f468156affaa9da840f620a75519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3beab172d4621bb9a227d6b12b767173191480d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
